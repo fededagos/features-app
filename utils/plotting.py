0066841aa065567163d143e4e7098eee139349b4
fededagos <fede.dagos@gmail.com>
2022-09-16 17:08:57 +0200

    Grey out invalid points

--- utils/plotting.py
+++ utils/plotting.py
@@ -25,32 +25,41 @@ def make_figure(
     fig = go.Figure()
     colors = ["orange", "blue", "green", "brown", "red", "purple"]
     color_dict = dict(zip(["PkC_cs", "PkC_ss", "GoC", "GrC", "MLI", "MFB"], colors))
+    good_neurons = new_df[new_df["color"] != "gray"]
+    grey_neurons = new_df[new_df["color"] == "gray"]
     for i, label in enumerate(["PkC_cs", "PkC_ss", "GoC", "GrC", "MLI", "MFB"]):
-
-        custom_data = list(
+        
+        custom_data_good = list(
             zip(
-                new_df[new_df["label"] == label]["dp"].to_numpy(),
-                new_df[new_df["label"] == label]["unit"].to_numpy(),
-                new_df[new_df["label"] == label]["raw_value"].to_numpy(),
-                new_df[new_df["label"] == label]["color"].to_numpy(),
+                good_neurons[good_neurons["label"] == label]["dp"].to_numpy(),
+                good_neurons[good_neurons["label"] == label]["unit"].to_numpy(),
+                good_neurons[good_neurons["label"] == label]["raw_value"].to_numpy(),
+                good_neurons[good_neurons["label"] == label]["color"].to_numpy(),
             )
         )
-
+        
+        custom_data_grey = list(
+            zip(
+                grey_neurons[grey_neurons["label"] == label]["dp"].to_numpy(),
+                grey_neurons[grey_neurons["label"] == label]["unit"].to_numpy(),
+                grey_neurons[grey_neurons["label"] == label]["raw_value"].to_numpy(),
+                grey_neurons[grey_neurons["label"] == label]["color"].to_numpy(),
+            )
+        )
+        
         fig.add_trace(
             go.Box(
-                y=new_df[new_df["label"] == label]["normalised_value"].to_numpy()
+                y=good_neurons[good_neurons["label"] == label]["normalised_value"].to_numpy()
                 if normalised
-                else new_df[new_df["label"] == label]["raw_value"].to_numpy(),
-                x=new_df[new_df["label"] == label]["feature"].to_numpy(),
+                else good_neurons[good_neurons["label"] == label]["raw_value"].to_numpy(),
+                x=good_neurons[good_neurons["label"] == label]["feature"].to_numpy(),
                 name=f'{label} (n = {new_df["label"].value_counts()[label] // len(np.unique(new_df["feature"].to_numpy()))})',
                 marker_color=colors[i],
-                boxpoints="all",
-                jitter=0.6,
-                pointpos=0,
+                offsetgroup=label,
                 legendgroup=label,
                 text=[label]
-                * len(new_df[new_df["label"] == label]["normalised_value"].to_numpy()),
-                customdata=custom_data,
+                * len(good_neurons[good_neurons["label"] == label]["normalised_value"].to_numpy()),
+                customdata=custom_data_good,
                 hovertemplate="<b> %{text} </b><br><br>"
                 + "Feature: %{x}<br>"
                 + "Raw value: %{customdata[2]}<br>"
@@ -58,6 +67,63 @@ def make_figure(
                 + "<extra></extra>",
             )
         )
+        
+        fig.add_trace(
+            go.Box(
+                y=good_neurons[good_neurons["label"] == label]["normalised_value"].to_numpy()
+                if normalised
+                else good_neurons[good_neurons["label"] == label]["raw_value"].to_numpy(),
+                x=good_neurons[good_neurons["label"] == label]["feature"].to_numpy(),
+                name=f'{label} (n = {new_df["label"].value_counts()[label] // len(np.unique(new_df["feature"].to_numpy()))})',
+                marker_color=colors[i],
+                boxpoints="all",
+                jitter=0.6,
+                pointpos=0,
+                offsetgroup=label,
+                legendgroup=label,
+                # legendgrouptitle=dict(text=label),
+                line = dict(color = 'rgba(0,0,0,0)'),
+                fillcolor = 'rgba(0,0,0,0)',
+                showlegend=False,
+                text=[label]
+                * len(good_neurons[good_neurons["label"] == label]["normalised_value"].to_numpy()),
+                customdata=custom_data_good,
+                hovertemplate="<b> %{text} </b><br><br>"
+                + "Feature: %{x}<br>"
+                + "Raw value: %{customdata[2]}<br>"
+                + "Source: %{customdata[0]}, unit %{customdata[1]}"
+                + "<extra></extra>",
+            )
+        )
+        
+        fig.add_trace(
+            go.Box(
+                y=grey_neurons[grey_neurons["label"] == label]["normalised_value"].to_numpy()
+                if normalised
+                else grey_neurons[grey_neurons["label"] == label]["raw_value"].to_numpy(),
+                x=grey_neurons[grey_neurons["label"] == label]["feature"].to_numpy(),
+                name=f'{label} (n = {new_df["label"].value_counts()[label] // len(np.unique(new_df["feature"].to_numpy()))})',
+                marker_color="gray",
+                boxpoints="all",
+                jitter=0.6,
+                pointpos=0,
+                offsetgroup=label,
+                legendgroup=label,
+                # legendgrouptitle=dict(text=label),
+                line = dict(color = 'rgba(0,0,0,0)'),
+                fillcolor = 'rgba(0,0,0,0)',
+                showlegend=False,
+                text=[label]
+                * len(grey_neurons[grey_neurons["label"] == label]["normalised_value"].to_numpy()),
+                customdata=custom_data_grey,
+                hovertemplate="<b> %{text} </b><br><br>"
+                + "Feature: %{x}<br>"
+                + "Raw value: %{customdata[2]}<br>"
+                + "Source: %{customdata[0]}, unit %{customdata[1]}"
+                + "<extra></extra>",
+            )
+        )
+        
 
     fig.update_layout(
         title=go.layout.Title(
@@ -75,8 +141,8 @@ def make_figure(
     fig.update_traces(
         marker=dict(size=1, line=dict(width=2, color="DarkSlateGrey")),
         selector=dict(mode="markers"),
-    )  #
-
+    ) 
+    
     fig.update_traces(hoverinfo="none", hovertemplate=None)
 
     return fig
@@ -90,8 +156,9 @@ def update_on_click(
     subselect=None,
 ):
     # This is to remove previously added highlighted points!
-    if len(fig.data) > 6:
-        fig.data = fig.data[:6]
+    # Why 6*3? Because there are 3 traces per cell type (1 for box, 1 for points, 1 for zero values)
+    if len(fig.data) > 6*3:
+        fig.data = fig.data[:6*3]
 
     n_neurons = np.unique(new_df["unit"].to_numpy()).shape[0]
     if np.array((which == "temporal")).any():
@@ -118,32 +185,6 @@ def update_on_click(
                 highlighted_point["color"].to_numpy(),
             )
         )
-        # fig.add_trace(
-        #     go.Box(
-        #         y=highlighted_point["normalised_value"].to_numpy()
-        #         if normalised
-        #         else highlighted_point["raw_value"].to_numpy(),
-        #         x=highlighted_point["feature"].to_numpy(),
-        #         marker=dict(size=12, line=dict(width=2, color="DarkSlateGrey")),
-        #         marker_color=color_dict[highlighted_label],
-        #         name=f"{highlighted_label} {subselect}",
-        #         boxpoints="all",
-        #         line=dict(color="rgba(0,0,0,0)"),
-        #         fillcolor="rgba(0,0,0,0)",
-        #         jitter=0.6,
-        #         pointpos=0,
-        #         legendgroup=highlighted_label,
-        #         # showlegend=False,
-        #         text=[highlighted_label]
-        #         * len(highlighted_point["normalised_value"].to_numpy()),
-        #         customdata=highlighted_custom_data,
-        #         hovertemplate="<b> %{text} </b><br><br>"
-        #         + "Feature: %{x}<br>"
-        #         + "Raw value: %{highlighted_customdata[2]}<br>"
-        #         + "Source: %{highlighted_customdata[0]}, unit %{highlighted_customdata[1]}"
-        #         + "<extra></extra>",
-        #     )
-        # )
         fig.add_trace(
             go.Scatter(
                 y=highlighted_point["normalised_value"].to_numpy()
@@ -151,10 +192,10 @@ def update_on_click(
                 else highlighted_point["raw_value"].to_numpy(),
                 x=highlighted_point["feature"].to_numpy(),
                 marker=dict(size=12, line=dict(width=2, color="DarkSlateGrey")),
-                marker_color=color_dict[highlighted_label],
+                marker_color=highlighted_point["color"],
                 name=f"{highlighted_label} {subselect}",
                 legendgroup=highlighted_label,
-                line=dict(dash="dot"),
+                line=dict(dash="dot", color=highlighted_point["color"].to_numpy()[0]),
                 # showlegend=False,
                 text=[highlighted_label]
                 * len(highlighted_point["normalised_value"].to_numpy()),
