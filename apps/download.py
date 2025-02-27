d733c467974fb7e901f3980303dbd4b56c5b6280
fededagos <fede.dagos@gmail.com>
2025-02-27 17:17:15 +0100

    Feedback from google docs

--- apps/download.py
+++ apps/download.py
@@ -17,8 +17,10 @@ layout = html.Div(
                 ),
                 dcc.Markdown(
                     """
-                    Excerpt from Figure 4 of Beau et al. (2025), showing summary peak-channel waveform and autocorrelograms for the ground-truth optotagged neurons in the database.
-                    The ground-truth units can be found in the "Hausser" and "Hull labelled" datasets for download below.
+                    Excerpt from Figure 4 of [Beau et al. (2025)](https://doi.org/10.1016/j.cell.2025.01.041), showing 
+                    summary peak-channel waveform and autocorrelograms for the ground-truth optotagged neurons in the 
+                    database. The ground-truth units can be found in the *Häusser lab* and *Hull lab* ground truth 
+                    datasets for download below.
                     
                     ---
                     All C4 datasets are in binary `.h5` format. 
@@ -31,12 +33,12 @@ layout = html.Div(
         html.Div(
             className="dataset-container",
             children=[
-                html.H2("Hausser dataset"),
+                html.H2("Häusser lab ground truth dataset"),
                 html.P(
-                    "Contains both labelled (optotagged and crosscorrelogram identified) and unlabelled neurons recorded by the Hausser lab at UCL. Neurons are recorded in the mouse using Neuropixels 1.0 and 2.0 probes."
+                    "Contains both labelled (optotagged and crosscorrelogram identified) and unlabelled neurons recorded by the Häusser lab at UCL. Neurons are recorded in the mouse using Neuropixels 1.0 and 2.0 probes."
                 ),
                 html.A(
-                    "Download Hausser Dataset",
+                    "Download Häusser lab Dataset",
                     id="btn-1",
                     href="https://rdr.ucl.ac.uk/ndownloader/files/43102990",
                     className="download-button",
@@ -47,12 +49,12 @@ layout = html.Div(
         html.Div(
             className="dataset-container",
             children=[
-                html.H2("Hull labelled dataset"),
+                html.H2("Hull lab ground truth dataset"),
                 html.P(
                     "Contains labelled (optotagged and crosscorrelogram identified) neurons recorded by the Hull lab at Duke. Neurons are recorded in the mouse using Neuropixels 1.0 probes."
                 ),
                 html.A(
-                    "Download Hull labelled dataset",
+                    "Download Hull lab labelled dataset",
                     id="btn-2",
                     href="https://rdr.ucl.ac.uk/ndownloader/files/43102996",
                     className="download-button",
@@ -63,12 +65,12 @@ layout = html.Div(
         html.Div(
             className="dataset-container",
             children=[
-                html.H2("Lisberger dataset"),
+                html.H2("Lisberger lab dataset"),
                 html.P(
-                    "Contains both labelled (expert-identified) and unlabelled neurons recorded by the Lisberger lab at Duke. Neurons are recorded in the monkey using a variety of electrodes."
+                    "Contains both human expert-labelled and unlabelled neurons recorded by the Lisberger lab at Duke. Neurons are recorded in the monkey using several different electrode types."
                 ),
                 html.A(
-                    "Download Lisberger dataset",
+                    "Download Lisberger lab dataset",
                     id="btn-3",
                     href="https://rdr.ucl.ac.uk/ndownloader/files/41721090",
                     className="download-button",
@@ -79,12 +81,12 @@ layout = html.Div(
         html.Div(
             className="dataset-container",
             children=[
-                html.H2("Hull unlabelled dataset"),
+                html.H2("Hull lab unlabelled dataset"),
                 html.P(
                     "Contains unlabelled neurons recorded by the Hull lab at Duke. Neurons are recorded in the mouse using Neuropixels 1.0 probes."
                 ),
                 html.A(
-                    "Download Hull unlabelled dataset",
+                    "Download Hull lab unlabelled dataset",
                     id="btn-4",
                     href="https://rdr.ucl.ac.uk/ndownloader/files/41720901",
                     className="download-button",
@@ -95,12 +97,12 @@ layout = html.Div(
         html.Div(
             className="dataset-container",
             children=[
-                html.H2("Medina unlabelled dataset"),
+                html.H2("Medina lab unlabelled dataset"),
                 html.P(
                     "Contains unlabelled neurons recorded by the Medina lab at Baylor College of Medicine. Neurons are recorded in the mouse using Neuropixels 1.0 probes."
                 ),
                 html.A(
-                    "Download Medina unlabelled dataset",
+                    "Download Medina lab unlabelled dataset",
                     id="btn-5",
                     href="https://rdr.ucl.ac.uk/ndownloader/files/42117129",
                     className="download-button",
